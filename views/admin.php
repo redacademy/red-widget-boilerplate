52f9c76f86b1bb2760d26f96a9c2dccb906e0b9f
Mandi Wise <hello@mandiwise.com>
2015-10-23 13:51:49 -0700

    Initial commit.

---
+++ views/admin.php
@@ -0,0 +1,9 @@
+<!-- This file is used to markup the administration form of the widget. -->
+
+<div class="widget-content">
+   <p><label for="<?php echo $this->get_field_id('title'); ?>">Title:</label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>">
+   </p>
+
+
+</div>
